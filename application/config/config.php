95a197e0ac92a770b9d23d5fb597e5f41e6cceab
dikyanwar22 <dikyanwar22@gmail.com>
2024-07-06 09:02:28 +0700

    .

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tesDikyAnwar/';
+$config['base_url'] = 'http://localhost/teswebLYG/';
 
 /*
 |--------------------------------------------------------------------------
